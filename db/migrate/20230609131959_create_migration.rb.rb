82199d8e42c52515e9bed441ad7879887da24877
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-09 16:22:06 +0300

    completed lab

---
+++ db/migrate/20230609131959_create_migration.rb.rb
@@ -0,0 +1,15 @@
+class CreateMigration.rb < ActiveRecord::Migration[6.1]
+  def change
+    class CreateMovies < ActiveRecord::Migration[6.1]
+      def change
+        create_table :movies do |t|
+          t.string :title
+          t.integer :release_date
+          t.string :director
+          t.string :lead
+          t.boolean :in_theaters
+        end
+      end
+    end
+  end
+end
